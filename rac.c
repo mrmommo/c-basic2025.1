f35ddfe9ae54ca1e646aedb815ae859da2a0fcf6
MrMommohy <long16102006@gmail.com>
2025-12-29 22:57:32 +0700

    Xoa rac.c

--- rac.c
+++ rac.c
@@ -1,38 +1,53 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX 1000
+#define MAX 901
 
-int stack[MAX];
-int top = -1;
+typedef struct {
+    int a, b, steps;
+} State;
 
-void push(int v) {
-    if (top < MAX - 1) {
-        stack[++top] = v;
-    }
+int visited[MAX][MAX];
+
+int gcd(int x, int y) {
+    return y == 0 ? x : gcd(y, x % y);
 }
 
-void pop() {
-    if (top >= 0) {
-        printf("%d\n", stack[top--]);
-    } else {
-        printf("NULL\n");
+int minSteps(int a, int b, int c) {
+    if (c > a && c > b) return -1;
+    if (c % gcd(a, b) != 0) return -1;
+
+    memset(visited, 0, sizeof(visited));
+    State queue[MAX * MAX];
+    int front = 0, rear = 0;
+
+    queue[rear++] = (State){0, 0, 0};
+
+    while (front < rear) {
+        State curr = queue[front++];
+        int x = curr.a, y = curr.b;
+
+        if (x == c || y == c) return curr.steps;
+        if (visited[x][y]) continue;
+        visited[x][y] = 1;
+
+        queue[rear++] = (State){a, y, curr.steps + 1};
+        queue[rear++] = (State){x, b, curr.steps + 1};
+        queue[rear++] = (State){0, y, curr.steps + 1};
+        queue[rear++] = (State){x, 0, curr.steps + 1};
+        int pour = (x < b - y) ? x : (b - y);
+        queue[rear++] = (State){x - pour, y + pour, curr.steps + 1};
+        pour = (y < a - x) ? y : (a - x);
+        queue[rear++] = (State){x + pour, y - pour, curr.steps + 1};
     }
+
+    return -1;
 }
 
 int main() {
-    char line[100];
-    while (fgets(line, sizeof(line), stdin)) {
-        if (line[0] == '#') break;
-
-        if (strncmp(line, "PUSH", 4) == 0) {
-            int v;
-            sscanf(line + 5, "%d", &v);
-            push(v);
-        } else if (strncmp(line, "POP", 3) == 0) {
-            pop();
-        }
-    }
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", minSteps(a, b, c));
     return 0;
 }
--- tuan6/b1(WATER_JUGS).c
+++ tuan6/b1(WATER_JUGS).c
@@ -1,12 +1,70 @@
 #include <stdio.h>
-int a , b , c ;
-//
+//queue
+int class[901][901] = {0};
+int queue1[901];
+int queue2[901];
+int top = 0 ;
+void push( int u, int v){
+    if( top < max ){
+        queue1[top++] = u;
+        queue2[top++] = v;
+    }
+}
+void pop(){
+    if ( top > 0){
+        for( int i = 0 ; i < top ; i++){
+            queue1[i] = queue1[i+1];
+            queue2[i] = queue2[i+1];
+        }
+        top--;
+    }
+}
+//gcd
+int gcd( int a ,int b){
+    if(b ==0) return a;
+    return gcd(b,a%b); 
+}
+int max(int a, int b){
+    return a > b ? a : b ;
+}
+//check
+int Check(int a, int b ,int c){
+    if(c <= max(a,b) && c % gcd(a,b) == 0) return 1;
+    else return 0;
+}
+//wtj
+int waterjugs( int a, int b , int c){
+    if (!Check(a,b,c)) return 0;
+    else{
+        int b1 = 0 , b2 = 0;
+        while(1){
+            pop();
+            
 
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
 
 
 
 int main(){
+    queue1[0] = 0 , queue2[0] = 0;
+    int a , b ,c ;
     scanf("%d %d %d", &a,&b,&c);
-    
-    
+    printf("%d\n", waterjugs(a,b,c));
+    return 0;
 }
